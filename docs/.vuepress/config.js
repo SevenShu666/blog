093f6a76e994a0b01cf6ea888bbc9b7f8af1c87c
shuguokun <shuguokun@123456.com>
2022-11-24 14:58:53 +0800

    blog创建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: "小草屋",
+  description: "藏点书",
+  themeConfig: {
+    nav: [
+      { text: "主页", link: "/" },
+      {
+        text: "前端",
+        items: [
+          { text: "html", link: "/web/html/" },
+          { text: "css", link: "/web/css/" },
+          { text: "js", link: "/web/js/" },
+        ],
+      },
+      { text: "node", link: "/node/" },
+      { text: "算法", link: "/arithmetic/" },
+    ],
+    sidebar: {
+      "/web/html/": [
+        ["", "第一个"],
+        ["one", "第二个"],
+        ["two", "第三个"],
+      ],
+    },
+  },
+};
